303a8cef76e25d332fe84ce45286cf281b1d39df
collins-kibet-ngenoh <collins.kibet.ngenoh@gmail.com>
2024-04-26 18:09:33 +0300

    basic lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 function NavBar() {
   return (
     <nav>
@@ -15,16 +14,19 @@ function Home() {
     </div>
   );
 }
-
-{/* write an <About> component here */}
-
-
+function About(){
+    return(
+        <div id="about">
+            <h2>About</h2>
+        </div>
+    )
+}
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
